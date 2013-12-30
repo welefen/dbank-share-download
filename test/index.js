425f9171f98940e51d771e278adf8ac3e1716cc2
welefen <welefen@gmail.com>
2013-12-30 11:16:15 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,11 +1,13 @@
 var share = require("../index.js");
-//含有目录
-var url = "http://dl.dbank.com/c03rw9ft0g";
+//多个目录
+var url1 = "http://dl.vmall.com/c0ukteanvy";
+//含有一个目录
+var url2 = "http://dl.vmall.com/c0k01h10pf";
 //含有多个文件
-var url = "http://dl.dbank.com/c0zmjhb94m";
+var url3 = "http://dl.vmall.com/c0p5uqph2m";
 //单个文件
-var url = "http://dl.dbank.com/c0s5q53e0a";
-share.download("http://dl.dbank.com/c0s5q53e0a", "./1/").then(function(){
+var url4 = "http://dl.dbank.com/c0s5q53e0a";
+share.download(url4, "./1/").then(function(){
 	console.log("下载完成");
 }).otherwise(function(error){
 	console.log(error);
